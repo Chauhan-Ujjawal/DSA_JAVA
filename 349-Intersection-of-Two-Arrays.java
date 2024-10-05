ed09dedec00536b5d2c17278d7e312669344a5e7
Chauhan-Ujjawal <ujjaval765@gmail.com>
2024-10-06 00:54:58 +0530

    [349] [Time Beats: 96.66%] [Memory Beats: 9.60%] - LeetPush

--- 349-Intersection-of-Two-Arrays.java
+++ 349-Intersection-of-Two-Arrays.java
@@ -1,31 +1,32 @@
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
-        int n1=nums1.length;
-        int n2=nums2.length;
-        ArrayList <Integer> result = new ArrayList <>();
-        int i=0;
-        int j=0;
-        while (i < n1 && j < n2) {
-            if (nums1[i] < nums2[j]) {
-                i++;
-            } else if (nums1[i] > nums2[j]) {
-                j++;
-            } else {
-                // Found an intersection
-                if (!result.contains(nums1[i])) {
-                    result.add(nums1[i]);
-                }
-                i++;
-                j++;
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> resultSet = new HashSet<>();
+
+        // Add elements from nums1 to the HashSet
+        for (int i = 0; i < nums1.length; i++) {
+            set1.add(nums1[i]);
+        }
+
+        // Check for intersections with nums2
+        for (int i = 0; i < nums2.length; i++) {
+            if (set1.contains(nums2[i])) {
+                resultSet.add(nums2[i]); // Add to result if found
             }
         }
 
-        int[] nums3 = new int[result.size()];
-        for (int k = 0; k < result.size(); k++) {
-            nums3[k] = result.get(k);
+        // Convert the resultSet to an int array
+        int[] nums3 = new int[resultSet.size()]; // Create an array of the same size as resultSet
+        Object[] tempArray = resultSet.toArray(); // Convert HashSet to Object array
+        
+        // Loop to fill the nums3 array using a traditional for loop
+        for (int i = 0; i < nums3.length; i++) {
+            nums3[i] = (int) tempArray[i]; // Cast each element to int and assign it
         }
-        return nums3;        
+
+        return nums3; // Return the filled array
     }
 }
