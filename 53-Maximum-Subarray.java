66578dbef203f98fbd4f62390daaa30c1e604487
Chauhan-Ujjawal <ujjaval765@gmail.com>
2024-11-24 19:42:55 +0530

    [53] [Time Beats: 99.41%] [Memory Beats: 9.18%] - LeetPush

---
+++ 53-Maximum-Subarray.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxsum=Integer.MIN_VALUE;
+        int currentsum=0;
+        //kadane algo
+        for(int i=0;i<nums.length;i++){   
+            currentsum+=nums[i];
+            maxsum=Math.max(maxsum,currentsum);
+            if(currentsum<0) currentsum=0;           
+        }
+        return maxsum;       
+    }
+}
