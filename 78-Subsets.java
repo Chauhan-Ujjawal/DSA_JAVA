b0cfb5ae556c2425503446743d81e7ce3ffb58e5
Chauhan-Ujjawal <ujjaval765@gmail.com>
2024-11-29 18:58:00 +0530

    [78] [Time Beats: 100.00%] [Memory Beats: 69.50%] - LeetPush

--- 78-Subsets.java
+++ 78-Subsets.java
@@ -1,19 +1,16 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        int n = nums.length;
-        int totalSubsets = 1 << n;  // 2^n subsets
-
-        for (int i = 0; i < totalSubsets; i++) {
-            List<Integer> subset = new ArrayList<>();
-            for (int j = 0; j < n; j++) {
-                // Check if the j-th bit is set in i
-                if ((i & (1 << j)) != 0) {
-                    subset.add(nums[j]);
-                }
-            }
-            result.add(subset);
+        //creating a list of list of integers to store our final answer 
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(res, new ArrayList<>(),nums , 0);
+        return res;        
+    }
+    private void backtrack(List<List<Integer>> res, List<Integer> temp , int[] nums ,int index){
+        res.add(new ArrayList<>(temp));
+        for(int i=index;i<nums.length;i++){
+            temp.add(nums[i]);
+            backtrack(res,temp,nums , i+1);
+            temp.remove(temp.size()-1);
         }
-        return result;
     }
 }
