6ce25f569c7e5cb49c173bb314cabdf2e4400646
UJJAWAL CHAUHAN <ujjaval765@gmail.com>
2025-01-14 23:28:26 +0530

    Time: 3 ms (6.14%), Space: 41.8 MB (30.64%) - LeetHub

---
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean checkRecord(String s) {
+        HashMap<Character, Integer> freq = new HashMap<>();
+        if(s.contains("LLL")) return false;
+        for(int i=0;i<s.length();i++){
+            char temp=s.charAt(i);
+            freq.put(temp, freq.getOrDefault(temp, 0) + 1);
+        }
+        int A = freq.getOrDefault('A', 0);
+        int L = freq.getOrDefault('L', 0);
+        if( A<2) return true;
+        return false; 
+    }
+}
