fbb223a10346c430c1c359686ddb95b97235f9a6
UJJAWAL CHAUHAN <ujjaval765@gmail.com>
2025-08-28 01:50:32 +0530

    Time: 43 ms (85.02%), Space: 112.9 MB (73.96%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.java
@@ -0,0 +1,71 @@
+
+
+class LRUCache {
+
+    class Node {
+        int key, value;
+        Node prev, next;
+
+        Node(int k, int v) {
+            key = k;
+            value = v;
+        }
+    }
+
+    private final int capacity;
+    private final Map<Integer, Node> map;
+    private final Node head, tail;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        map = new HashMap<>();
+
+        head = new Node(0, 0); 
+        tail = new Node(0, 0); 
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public int get(int key) {
+        if (!map.containsKey(key)) return -1;
+
+        Node node = map.get(key);
+        moveToFront(node);
+        return node.value;
+    }
+
+    public void put(int key, int value) {
+        if (map.containsKey(key)) {
+            Node existing = map.get(key);
+            existing.value = value;
+            moveToFront(existing);
+        } else {
+            if (map.size() == capacity) {
+                Node lru = tail.prev;
+                remove(lru);
+                map.remove(lru.key);
+            }
+            Node newNode = new Node(key, value);
+            insertToFront(newNode);
+            map.put(key, newNode);
+        }
+    }
+
+    private void moveToFront(Node node) {
+        remove(node);
+        insertToFront(node);
+    }
+
+    private void remove(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    private void insertToFront(Node node) {
+        node.next = head.next;
+        node.prev = head;
+
+        head.next.prev = node;
+        head.next = node;
+    }
+}
